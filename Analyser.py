1d9ee40f7bc0d831f3cd9d8c22bb97671df4c97b
xarkes <antidepetit@gmail.com>
2016-04-01 16:45:10 +0200

    Initial commit

---
+++ Analyser.py
@@ -0,0 +1,71 @@
+from flask import abort
+from hashlib import sha256, md5
+from datetime import datetime
+from bson.objectid import ObjectId, InvalidId
+import subprocess
+import ssdeep
+import mowr
+
+class Analyser():
+    def __init__(self, mongo, file=None, id=None):
+        self.db = mongo.db
+        self.id = id
+        self.file = file
+        self.loadConfig()
+        # Check if id is valid
+        if id is not None:
+            try:
+                self.db.files.find_one_or_404({"_id": ObjectId(self.id)})
+            except InvalidId:
+                abort(404)
+
+        # Get file path
+        if self.file is None:
+            sha256sum = self.db.files.find_one_or_404({"_id": ObjectId(self.id)})['sha256']
+            self.file = mowr.getFileLocation(sha256sum)
+
+    def loadConfig(self):
+        #TODO Move it in the main and maybe git pull etc.
+        self.pmf_bin = '/home/antide/stage/php-malware-finder/php-malware-finder/phpmalwarefinder'
+
+    def analyse(self):
+        """ Returns the file _id """
+        # Compute hashes
+        with open(self.file, 'rb') as f:
+            buf = f.read()
+        sha256sum = sha256(buf).hexdigest()
+        md5sum = md5(buf).hexdigest()
+        ssdeephash = ssdeep.hash(buf)
+
+        # Start the analysis
+        # TODO yara bindings
+        analysis = subprocess.check_output(
+                [self.pmf_bin, self.file]
+                )
+        # Format it (I could have called awk too)
+        analysis = ' '.join([v for i, v in list(enumerate(analysis.split())) if i%2 == 0])
+
+        # Store the result into the database
+        if self.id is None:
+            data = {"first_analysis": datetime.utcnow().ctime(),
+                    "last_analysis": datetime.utcnow().ctime(),
+                    "md5": md5sum,
+                    "sha256": sha256sum,
+                    "ssdeep": ssdeephash,
+                    "pmf_analysis": analysis}
+            id = self.db.files.insert_one(data).inserted_id
+            self.id = id
+        else:
+            data = {"last_analysis": datetime.utcnow().ctime(),
+                    "md5": md5sum,
+                    "sha256": sha256sum,
+                    "ssdeep": ssdeephash,
+                    "pmf_analysis": analysis}
+            self.db.files.update_one({"_id": ObjectId(self.id)}, {"$set": data})
+
+        return self.id
+
+    def getInfos(self):
+        """ Get current analysis informations """
+        return self.db.files.find_one_or_404({"_id": ObjectId(self.id)})
+
---
+++ mowr.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request, redirect, abort
+from flask.ext.pymongo import PyMongo
+from hashlib import sha256
+from shutil import move
+import Analyser
+import os
+
+""" Configuration """
+app = Flask(__name__)
+app.config['TMP_FOLDER'] = '/tmp/uploads'
+app.config['UPLOAD_FOLDER'] = '/tmp/uploads/lulz'
+mongo = PyMongo(app)
+
+""" Custom functions """
+def getFileLocation(sha256sum):
+    return '{0}/{1}'.format(app.config['UPLOAD_FOLDER'], sha256sum)
+
+""" Routes """
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Save file in temp folder
+    file = request.files['file']
+
+    #TODO check file length
+    #Add tags
+    #todo hash cote client
+
+    # Check user input
+    path = os.path.join(app.config['TMP_FOLDER'], file.filename)
+    if os.path.isdir(path):
+        #TODO add flash error
+        return redirect('/')
+
+    file.save(path)
+    file = path
+
+    # Check the file sha256 and if it already exists
+    with open(file, 'rb') as f:
+        buf = f.read()
+    sha256sum = sha256(buf).hexdigest()
+    f = mongo.db.files.find_one({"sha256": sha256sum})
+
+    # If already exists, delete the old one and ask what to do
+    if f is not None:
+        id = f["_id"]
+        os.remove(file)
+        return redirect('/file/%s/choice' % id)
+
+    # If it is the first time, save the file to the correct location and delete the old one
+    newfile = getFileLocation(sha256sum)
+    move(file, newfile)
+
+    # Then analyse it and show results
+    analyser = Analyser.Analyser(mongo, newfile)
+    id = analyser.analyse()
+    return redirect('/file/%s/analysis' % id)
+
+
+@app.route('/file/<id>/<action>', methods=['GET', 'POST'])
+def file(id, action):
+    # Init analyser to check the id
+    analyser = Analyser.Analyser(mongo, None, id)
+
+    # Handle action
+    if action == 'choice':
+        return render_template('choose.html', id=id)
+    elif action == 'analysis':
+        f = analyser.getInfos()
+        return render_template('result.html', file=f)
+    elif action == 'reanalyse':
+        analyser.analyse()
+        f = analyser.getInfos()
+        return render_template('result.html', file=f)
+    else:
+        abort(404)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+""" Error handlers """
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    if not os.access(app.config['TMP_FOLDER'], os.W_OK) or not os.access(app.config['UPLOAD_FOLDER'], os.W_OK):
+        print("Either TMP_FOLDER or UPLOAD_FOLDER is not writable. Please update the configuration.")
+
+
+    app.debug = True
+    app.run()
+
