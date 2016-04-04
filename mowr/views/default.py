af6315bc1e97bd305906342fc6437c066e55bd14
xarkes <antidepetit@gmail.com>
2016-04-04 16:15:03 +0200

    Added tags

--- mowr/views/default.py
+++ mowr/views/default.py
@@ -3,12 +3,20 @@ from hashlib import sha256
 from flask import render_template, request, redirect, abort, url_for, flash, Blueprint, current_app
 
 from mowr.model.analyser import Analyser
+from random import choice
 
 default = Blueprint('default', __name__)
 
 def errorUpload():
     flash('There was an error while uploading the file. Please try with a different file.', 'danger')
     return redirect(url_for('default.index'))
+
+# TODO
+def tagnameToColor(tag):
+    return choice(['primary', 'danger', 'success', 'default', 'warning'])
+
+def formatTag(soft, tag):
+    return '<a class="label label-' + tagnameToColor(tag) + '" href="' + url_for('default.tag', soft=soft, tag=tag) + '">' + tag +'</a>'
 
 @default.route('/upload', methods=['POST'])
 def upload():
@@ -59,13 +67,20 @@ def file(id, action):
     if action == 'choose':
         return render_template('choose.html', id=id)
     elif action == 'analysis':
-        # TODO Add tags for pmf answer (+bootstrap)
         f = analyser.getInfos()
-        return render_template('result.html', file=f)
+        return render_template('result.html', file=f, formatTag=formatTag)
     elif action == 'reanalyse':
         analyser.analyse()
         f = analyser.getInfos()
         return render_template('result.html', file=f)
+    else:
+        abort(404)
+
+@default.route('/tag/<soft>/<tag>')
+def tag(soft, tag):
+    if soft == 'pmf':
+        l = current_app.mongo.db.files.find({"pmf_analysis": {"$regex": ".*" + tag + ".*"}}).limit(10)
+        return render_template('tag.html', files=l, formatTag=formatTag)
     else:
         abort(404)
 
