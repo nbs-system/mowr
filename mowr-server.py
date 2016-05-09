58600eee8e3ab728fc485901dccd780546912fc3
xarkes <antidepetit@gmail.com>
2016-05-09 12:05:48 +0200

    Documentation update

--- mowr-server.py
+++ mowr-server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import logging
 
 from mowr import create_app
 
@@ -17,9 +18,14 @@ def run(host, port):
 
     # Avoid creating a new key each time a file is modified
     if app.debug:
-        print("WARNING The application is running with debugging enabled. Please deactivate it if it's not needed !")
-        app.config['SECRET_KEY'] = 'rZy9tp8G8EvtBap2cE1ibzUfhaNNJXS76InlXfcME1clVQYek5jl6hS8+TRWleAgoZGjXEKCHCPh2idlTLGsE9lIas2fa5DCNNo1UvGkKKpeUJqQ+/f9nYvLUMeaJkShV5j/GsTCa8ygNLd/Yn7DUyp2PbijNi/kqwUS9THRIWE='
+        logging.warning("==============================================")
+        logging.warning(
+            "The application is running with debugging enabled. Please deactivate it if it's not needed !")
+        logging.warning("==============================================")
+        app.config[
+            'SECRET_KEY'] = 'rZy9tp8G8EvtBap2cE1ibzUfhaNNJXS76InlXfcME1clVQYek5jl6hS8+TRWleAgoZGjXEKCHCPh2idlTLGsE9lIas2fa5DCNNo1UvGkKKpeUJqQ+/f9nYvLUMeaJkShV5j/GsTCa8ygNLd/Yn7DUyp2PbijNi/kqwUS9THRIWE='
     app.run(host=host, port=port)
+
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=5000)
